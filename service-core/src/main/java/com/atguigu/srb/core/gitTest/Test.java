eb35d6a8be30d3487cc688295b53ec537945caa5
liyi <1490791039@qq.com>
2021-07-06 20:43:37 +0800

    second commit

--- service-core/src/main/java/com/atguigu/srb/core/gitTest/Test.java
+++ service-core/src/main/java/com/atguigu/srb/core/gitTest/Test.java
@@ -3,5 +3,7 @@ package com.atguigu.srb.core.gitTest;
 public class Test {
     public static void main(String[] args) {
         System.out.println("........第一版");
+        System.out.println("........第二版");
+
     }
 }
